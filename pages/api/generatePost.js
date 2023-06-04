db98c951a2176f3520dc835a125888a1b67e5f71
Stephen Cunningham <stevecunningham@outlook.com>
2023-06-04 13:07:27 -0500

    upgraded ai, but not returning html

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -12,27 +12,109 @@ export default async function handler(req, res) {
 
     const openAi = new OpenAIApi(config); 
 
-    const topic = "Top 10 tips for Cat Oweners";
 
-    const keywords = "first-time cat owners, common cat health problems, best cat breeds"
+    const {topic, keywords} = req.body;
 
-    //chat gpt4 is not available yet
-    const response = await openAi.createCompletion({
-        model: "text-davinci-003",
-        temperature: 0, //zero is lowest risk, one is highest risk
-        max_tokens: 3600,
-        prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following keywords ${keywords}.
-        The content should be formatted in SEO friendly HTML.
-        The response must also include appropriate HTML title and meta description content.
-        The return format must be stringified JSON in the following format:
-        {
-            "postContent": post content here
-            "title": title goes here
-            "metaDescription": meta description here
-        }`,
-    });
+    
 
-    console.log("response", response);
-    res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")) })
+    //chat gpt4 is not publicly available yet
+    // const response = await openAi.createCompletion({
+    //     model: "text-davinci-003",
+    //     temperature: 0, //zero is lowest risk, one is highest risk
+    //     max_tokens: 3600,
+    //     prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following keywords ${keywords}.
+    //     The content should be formatted in SEO friendly HTML.
+    //     The response must also include appropriate HTML title and meta description content.
+    //     The return format must be stringified JSON in the following format:
+    //     {
+    //         "postContent": post content here
+    //         "title": title goes here
+    //         "metaDescription": meta description here
+    //     }`,
+    // });
+
+    //above is the text-davinci-003 model of OpenAI, but now we will use Chat Gpt 3.5 turbo model
+    //instead of createCompletion method there is a createChatCompletion
+    //also will not be return JSON but instead receive postContent, title, and metaDescription separately
+    //this will be more of a back and forth chat
+    
+    
+
+    const postContentResponse = await openAi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0, //do not need max tokens anymore default set to infinity
+        messages: [{ //no propmt just messages
+            role: "system", //tells openAI how to behave in content
+            content: "You are a blog post generator"
+        }, {
+            role:"user", 
+            content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following keywords ${keywords}. 
+            The content should be formatted in SEO friendly HTML, 
+            limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`
+
+        },]
+
+    })
+
+    const postContent = postContentResponse.data.choices[0]?.message?.content || "";
+
+    const titleResponse = await openAi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0, //do not need max tokens anymore default set to infinity
+        messages: [{ //no propmt just messages
+            role: "system", //tells openAI how to behave in content
+            content: "You are a blog post generator"
+        }, {
+            role:"user", 
+            content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following keywords ${keywords}. 
+            The content must be formatted in HTML, 
+            limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`
+
+        }, {
+            role: "assistant", //this is the response returned from openai
+            content: postContent
+    }, {
+        role: "user",
+        content: "Generate an appropriate title tag text for the above blog post"
+    },]
+
+    })
+
+
+    const metaDescriptionResponse =  await openAi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0, //do not need max tokens anymore default set to infinity
+        messages: [{ //no propmt just messages
+            role: "system", //tells openAI how to behave in content
+            content: "You are a blog post generator"
+        }, {
+            role:"user", 
+            content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following keywords ${keywords}. 
+            The content must be formatted in HTML, 
+            limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i`
+
+        }, {
+            role: "assistant", //this is the response returned from openai
+            content: postContent
+    }, {
+        role: "user",
+        content: "Generate SEO-friendly meta description content for the above blog post"
+    }]
+
+    })
+
+    const title = titleResponse.data.choices[0]?.message?.content || "";
+    const metaDescription = metaDescriptionResponse.data.choices[0]?.message?.content || "";
+
+    console.log("POST CONTENT", postContent);
+    console.log("TITLE", title);
+    console.log("META DESCR", metaDescription);
+    res.status(200).json({
+        post: {
+            postContent,
+            title,
+            metaDescription,
+        },
+    })
   }
   
--- pages/post/new.js
+++ pages/post/new.js
@@ -8,25 +8,61 @@ export default function NewPost() {
     //tailwind css comes with reset css out of the box for consistency across browsers
     //pages provide routes to this home page
 
-    const [postContent, setPostContent] = useState("");
 
-    const handleClick = async () => {
+    const [topic, setTopic] = useState("");
+    const [keywords, setKeywords] = useState("");
+    const [postContent, setPostContent] = useState("");
+    const [title, setTitle] = useState("");
+    const [ metaDescription, setMetaDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //prevent default posting of this form to itself
         //calls browser built in fetch call and is async because it returns a promis
         {/* @ts-expect-error Async Server Component */}
         const response = await fetch(`/api/generatePost`, {
             method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({topic, keywords}),
         });
         
         const json = await response.json();
-        console.log('RESULT', json.post.postContent);
         setPostContent(json.post.postContent);
+        setTitle(json.post.title);
+        setMetaDescription(json.post.metaDescription);
     }
       return <div> 
-        <h1>This is the new post page</h1>
-        <button className="btn" onClick={handleClick}>
+
+        <form onSubmit={handleSubmit}>
+            <div>
+                    <label>
+                        <strong>
+                            Generate a blog on the topic of:
+                        </strong>
+                    </label>
+                    <textarea className="resize-none border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(e) => setTopic(e.target.value)}/>
+            </div>
+            <div>
+            <label>
+                        <strong>
+                            Targeting the following keywords:
+                        </strong>
+                    </label>
+                    <textarea className="resize-none border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)}/>
+            </div>
+        <button type="submit" className="btn">
             Generate
         </button>
-        <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html: postContent}}></div>
+        </form>
+   
+        
+        <div className="max-w-screen-sm p-10" >
+       {/* dangerouslySetInnerHTML={{__html: postContent.title}} */}
+            <h1 >{title}</h1>
+            <h3>{postContent}</h3>
+            <h3>{metaDescription}</h3>
+        </div>
          </div>;
     }
     
